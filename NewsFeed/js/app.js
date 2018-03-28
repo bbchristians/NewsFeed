2fbe8d7ebed9f89c930ee077740372ddd4783913
Ben Christians <bbc7909@rit.edu>
2018-03-28 16:14:15 -0400

    Changed location of js

--- NewsFeed/js/app.js
+++ NewsFeed/js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation();
+
+function createBlankTemplate() {
+  $.get("../article.html", function callback(html_string) {
+    console.log(html_string);
+  });
+}
+
+
+function UpdateBBCWorldNews() {
+  $url = "http://feeds.bbci.co.uk/news/world/rss.xml";
+  $.ajax({
+    url: $url,
+    dataType: 'xml',
+    success: function(data) {
+      // Get all news items from data
+      $items = $(data).find("item");
+      // For each item in data, display
+      $.each( $items, function( index, value ) {
+        console.log($(value).find("title").text());
+      });
+    }
+  });
+}
